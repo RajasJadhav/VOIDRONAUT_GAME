21530c58868cfdc6e430a5d78d2ade2b2680945c
RajasJadhav <rajas.jadhav2604@gmail.com>
2026-04-28 22:08:01 +0530

    added pause menu

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,9 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    private bool isPaused = false;
    public void GoToScene(string sceneName)
     {
+        Time.timeScale = 1.0f;
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void TogglePause()
+    {
+        if (isPaused) Resume();
+        else Pause();
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
     }
     
 }
