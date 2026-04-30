eeb89bb5be770032124cdbd60a617acf118f6660
RajasJadhav <rajas.jadhav2604@gmail.com>
2026-04-30 15:03:42 +0530

    adding footsteps

--- Assets/Scripts/FootstepController.cs
+++ Assets/Scripts/FootstepController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class FootstepController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] audioClip;
 
-    // Update is called once per frame
-    void Update()
+    private float minPitch = 0.9f;
+    private float maxPitch = 1.1f;
+    public void PlayFootStep()
     {
-        
+
+        if (audioClip.Length == 0 || audioSource == null) return;
+
+        int index = Random.Range(0, audioClip.Length);
+
+        AudioClip clip = audioClip[index];
+
+        audioSource.pitch = Random.Range(minPitch, maxPitch);
+        audioSource.PlayOneShot(clip); 
     }
 }
